8a391db0b0fe664f20462084fb3a667044a3a7fc
Guan Shanyan <81640539+syguan96@users.noreply.github.com>
2023-04-07 10:34:03 +0800

    Prevent 'nan' caused by torch.acos

--- src/nemf/losses.py
+++ src/nemf/losses.py
@@ -6,22 +6,21 @@ class GeodesicLoss(nn.Module):
     def __init__(self):
         super(GeodesicLoss, self).__init__()
 
-    def compute_geodesic_distance(self, m1, m2):
+    def compute_geodesic_distance(self, m1, m2, epsilon=1e-7):
         """ Compute the geodesic distance between two rotation matrices.
-
         Args:
             m1, m2: Two rotation matrices with the shape (batch x 3 x 3).
-
         Returns:
             The minimal angular difference between two rotation matrices in radian form [0, pi].
         """
         batch = m1.shape[0]
-        m = torch.bmm(m1, m2.transpose(1, 2))  # batch*3*3
+        m = torch.bmm(m1, m2.permute(0, 2, 1))  # batch*3*3
 
         cos = (m[:, 0, 0] + m[:, 1, 1] + m[:, 2, 2] - 1) / 2
-        cos = torch.min(cos, torch.autograd.Variable(torch.ones(batch).cuda()))
-        cos = torch.max(cos, torch.autograd.Variable(torch.ones(batch).cuda()) * -1)
-
+        # cos = (m.diagonal(dim1=-2, dim2=-1).sum(-1) -1) /2
+        # cos = torch.min(cos, torch.autograd.Variable(torch.ones(batch).cuda()))
+        # cos = torch.max(cos, torch.autograd.Variable(torch.ones(batch).cuda()) * -1)
+        cos = torch.clamp(cos, -1 + epsilon, 1 - epsilon)
         theta = torch.acos(cos)
 
         return theta
@@ -34,4 +33,5 @@ class GeodesicLoss(nn.Module):
         elif reduction == 'none':
             return loss
         else:
+            raise RuntimeError(f'unsupported reduction: {reduction}')
             raise RuntimeError(f'unsupported reduction: {reduction}')
